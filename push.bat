e306b891c852664f5ecb2d29df6d09955703cad6
joelhowell <howell.joel.t@gmail.com>
2019-03-05 00:45:14 -0500

    Commit

--- push.bat
+++ push.bat
@@ -1,7 +1,25 @@
 @echo off
+title git
+SETLOCAL EnableDelayedExpansion
+for /F "tokens=1,2 delims=#" %%a in ('"prompt #$H#$E# & echo on & for %%b in (1) do     rem"') do (
+  set "DEL=%%a"
+)
+call :colorEcho e0 "[Git Status]"
+echo.
+
 git status
 pause
 git add . 
-set /p message=Commit Message:
-git commit -m %message%
+set /p m=Commit Message: 
+git commit -m %m%
 git push
+
+call :colorEcho a0 "[Push Complete]"
+echo.
+exit.
+
+:colorEcho
+echo off
+<nul set /p ".=%DEL%" > "%~2"
+findstr /v /a:%1 /R "^$" "%~2" nul
+del "%~2" > nul 2>&1i
