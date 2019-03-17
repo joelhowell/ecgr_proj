f7b2a326b4c924b418902bae28e8b0054c743209
joelhowell <howell.joel.t@gmail.com>
2019-03-17 16:05:02 -0400

    TEst

--- src/init.cpp
+++ src/init.cpp
@@ -15,10 +15,6 @@ namespace utils
         system(winTitle.c_str());
 
         //	Change to full screen
-       // SetConsoleDisplayMode(GetStdHandle(STD_OUTPUT_HANDLE), CONSOLE_FULLSCREEN_MODE, 0);
-       system("mode con COLS=700");
-       ShowWindow(GetConsoleWindow(),SW_MAXIMIZE);
-       SendMessage(GetConsoleWindow(),WM_SYSKEYDOWN,VK_RETURN,0x20000000);
-
+        SendMessage(GetConsoleWindow(), WM_SYSKEYDOWN, VK_RETURN, 0x20000000);
     }
 }
