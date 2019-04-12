4ad142dbf5ac7f29a8cd068cb05dd897f99172f3
joelhowell <howell.joel.t@gmail.com>
2019-04-11 23:48:42 -0400

    Modified Main

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,23 @@
 /************************************************************************************************************
- *																											*
- *		Project Name:		Battleship																		*
- *																											*
- *		Version:			1.0.0																			*
- *																											*
- *		Team Name:			Flaming Burritos																*
- *                                                                                                          *
- *      Senior Developer:   Joel H                                                                          *                                                           *
- *																											*
- *		Developers:			Will W, Kelby P, Richard O, and Abdul E.								        *
- *																											*
- *		Date Created:		3/5/2019																		*
- *																											*
- *		Last Modified:		4/11/2019																		*
- *																											*
- *		Purpose:            To play battleship :)															*
- *																											*
- *																											*
- ***********************************************************************************************************/										
+*																											*
+*		Project Name:       Battleship																		*
+*																											*
+*		Version:            1.0.0																			*
+*																											*
+*       Team Name:          Flaming Burritos																*
+*                                                                                                           *
+*       Senior Developer:   Joel H                                                                          *
+*																											*
+*		Developers:         Will W, Kelby P, Richard O, and Abdul E.								        *
+*																											*
+*		Date Created:       3/5/2019																		*
+*																											*
+*		Last Modified:      4/11/2019																		*
+*																											*
+*		Purpose:            To play battleship :)															*
+*																											*
+*																											*
+*************************************************************************************************************/	  
 
 #include "utils.h"
 #include <iostream>
